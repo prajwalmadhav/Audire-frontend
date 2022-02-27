a8debf9c8b62fd8fdc50b608e1cf96129eee9cb3
PG <prajwalsmadhav@gmail.com>
2022-02-27 21:32:10 +0530

    updates routes and fixed bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import * as React from "react"
-import { Route } from "react-router-dom";
 import {
   ChakraProvider,
   theme,
 } from "@chakra-ui/react"
 
+import { BrowserRouter as Router, Route,Link, Routes, Outlet } from 'react-router-dom'
+
+import Sidebar from "./Components/Sidebar";
 import Homepage from "./Components/Homepage";
 
 export const App = () => {
-  const curUrl = window.location.pathname
-  console.log(curUrl)
-  return (    
-    <Homepage children={undefined}/>
-    );
+  
+  return (
+    
+    <ChakraProvider theme={theme}>
+        
+      <Router>
+          <Sidebar children={undefined} />
+          
+        <Routes>
+          {/* Sidebar for the inclusive pages */}
+          <Route path='home' element={<Homepage />} />
+          <Route path='test' element={<Homepage/>} />
+          <Route path='pg' element={<Homepage/>} />
+
+        </Routes>
+
+      </Router>
+    </ChakraProvider>
+  );
 }
+
