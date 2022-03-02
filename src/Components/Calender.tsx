0cb91ed029ad1298bc4b844b52591219d21f85fd
PG <prajwalsmadhav@gmail.com>
2022-03-02 16:02:09 +0530

    added form for plan audit

--- src/Components/Calender.tsx
+++ src/Components/Calender.tsx
@@ -1,271 +1,293 @@
-import React from "react";
 import {
   chakra,
   Box,
-  Flex,
   useColorModeValue,
   SimpleGrid,
   GridItem,
-  Heading,
-  Text,
   Stack,
   FormControl,
   FormLabel,
   Input,
-  InputGroup,
-  InputLeftAddon,
-  FormHelperText,
-  Textarea,
-  Avatar,
-  Icon,
   Button,
-  VisuallyHidden,
-  Select,
-  Checkbox,
-  RadioGroup,
-  Radio,
+  Textarea,
+  Heading,
 } from "@chakra-ui/react";
-import { FaUser } from "react-icons/fa";
 
 export default function Component() {
   return (
-    <Box bg={useColorModeValue("gray.50", "inherit")} p={10} >
+    <>
       
-      
+      <Box bg={useColorModeValue("gray.30", "inherit")} p={10} ml={{ lg:'400px' }}  >
+        
+        
 
-      <Box mt={[10, 0]} ml={{ lg:'600px' }}>
-        <SimpleGrid
-          display={{ base: "initial", md: "grid" }}
-          columns={{ md: 3 }}
-          spacing={{ md: 6 }}
-        >
-         
-          <GridItem mt={[5, null, 0]} colSpan={{ md: 2 }}>
-            <chakra.form
-              method="POST"
-              shadow="base"
-              rounded={[null, "md"]}
-              overflow={{ sm: "hidden" }}
-            >
-              <Stack
-                px={4}
-                py={5}
-                p={[null, 6]}
-                bg={useColorModeValue("white", "gray.700")}
-                spacing={6}
+        <Box mt={[10, 0]} >
+          
+          <SimpleGrid
+            display={{ base: "initial", md: "grid", lg:"flex-end" }}
+            columns={{ md: 3 , lg: 4}}
+            spacing={{ md: 6 }}
+          >
+              
+            <GridItem mt={[5, null, 0]} colSpan={{ md: 2 }}>
+              <chakra.form
+                method="POST"
+                shadow="inner"
+                rounded={[null, "md"]}
+                overflow={{ sm: "hidden" }}
               >
-                <SimpleGrid columns={6} spacing={6}>
-                  <FormControl as={GridItem} colSpan={[6, 3]}>
-                    <FormLabel
-                      htmlFor="first_name"
-                      fontSize="sm"
-                      fontWeight="md"
-                      color={useColorModeValue("gray.700", "gray.50")}
-                    >
-                      First name
-                    </FormLabel>
-                    <Input
-                      type="text"
-                      name="first_name"
-                      id="first_name"
-                      autoComplete="given-name"
-                      mt={1}
-                      focusBorderColor="brand.400"
-                      shadow="sm"
-                      size="sm"
-                      w="full"
-                      rounded="md"
-                    />
-                  </FormControl>
-
-                  <FormControl as={GridItem} colSpan={[6, 3]}>
-                    <FormLabel
-                      htmlFor="last_name"
-                      fontSize="sm"
-                      fontWeight="md"
-                      color={useColorModeValue("gray.700", "gray.50")}
-                    >
-                      Last name
-                    </FormLabel>
-                    <Input
-                      type="text"
-                      name="last_name"
-                      id="last_name"
-                      autoComplete="family-name"
-                      mt={1}
-                      focusBorderColor="brand.400"
-                      shadow="sm"
-                      size="sm"
-                      w="full"
-                      rounded="md"
-                    />
-                  </FormControl>
-
-                  <FormControl as={GridItem} colSpan={[6, 3]}>
-                    <FormLabel
-                      htmlFor="first_name"
-                      fontSize="sm"
-                      fontWeight="md"
-                      color={useColorModeValue("gray.700", "gray.50")}
-                    >
-                      First name
-                    </FormLabel>
-                    <Input
-                      type="text"
-                      name="first_name"
-                      id="first_name"
-                      autoComplete="given-name"
-                      mt={1}
-                      focusBorderColor="brand.400"
-                      shadow="sm"
-                      size="sm"
-                      w="full"
-                      rounded="md"
-                    />
-                  </FormControl>
-
-                  <FormControl as={GridItem} colSpan={[6, 3]}>
-                    <FormLabel
-                      htmlFor="last_name"
-                      fontSize="sm"
-                      fontWeight="md"
-                      color={useColorModeValue("gray.700", "gray.50")}
-                    >
-                      Last name
-                    </FormLabel>
-                    <Input
-                      type="text"
-                      name="last_name"
-                      id="last_name"
-                      autoComplete="family-name"
-                      mt={1}
-                      focusBorderColor="brand.400"
-                      shadow="sm"
-                      size="sm"
-                      w="full"
-                      rounded="md"
-                    />
-                  </FormControl>
-
-                  <FormControl as={GridItem} colSpan={[6, 3]}>
-                    <FormLabel
-                      htmlFor="first_name"
-                      fontSize="sm"
-                      fontWeight="md"
-                      color={useColorModeValue("gray.700", "gray.50")}
-                    >
-                      First name
-                    </FormLabel>
-                    <Input
-                      type="text"
-                      name="first_name"
-                      id="first_name"
-                      autoComplete="given-name"
-                      mt={1}
-                      focusBorderColor="brand.400"
-                      shadow="sm"
-                      size="sm"
-                      w="full"
-                      rounded="md"
-                    />
-                  </FormControl>
-
-                  <FormControl as={GridItem} colSpan={[6, 3]}>
-                    <FormLabel
-                      htmlFor="last_name"
-                      fontSize="sm"
-                      fontWeight="md"
-                      color={useColorModeValue("gray.700", "gray.50")}
-                    >
-                      Last name
-                    </FormLabel>
-                    <Input
-                      type="text"
-                      name="last_name"
-                      id="last_name"
-                      autoComplete="family-name"
-                      mt={1}
-                      focusBorderColor="brand.400"
-                      shadow="sm"
-                      size="sm"
-                      w="full"
-                      rounded="md"
-                    />
-                  </FormControl>
-
-                  <FormControl as={GridItem} colSpan={[6, 3]}>
-                    <FormLabel
-                      htmlFor="first_name"
-                      fontSize="sm"
-                      fontWeight="md"
-                      color={useColorModeValue("gray.700", "gray.50")}
-                    >
-                      First name
-                    </FormLabel>
-                    <Input
-                      type="text"
-                      name="first_name"
-                      id="first_name"
-                      autoComplete="given-name"
-                      mt={1}
-                      focusBorderColor="brand.400"
-                      shadow="sm"
-                      size="sm"
-                      w="full"
-                      rounded="md"
-                    />
-                  </FormControl>
-
-                  <FormControl as={GridItem} colSpan={[6, 3]}>
-                    <FormLabel
-                      htmlFor="last_name"
-                      fontSize="sm"
-                      fontWeight="md"
-                      color={useColorModeValue("gray.700", "gray.50")}
-                    >
-                      Last name
-                    </FormLabel>
-                    <Input
-                      type="text"
-                      name="last_name"
-                      id="last_name"
-                      autoComplete="family-name"
-                      mt={1}
-                      focusBorderColor="brand.400"
-                      shadow="sm"
-                      size="sm"
-                      w="full"
-                      rounded="md"
-                    />
-                  </FormControl>
-
-                    
-                </SimpleGrid>
-              </Stack>
-              <Box
-                px={{ base: 4, sm: 6, lg: 6 }}
-                py={3}
-                bg={useColorModeValue("gray.50", "gray.900")}
-                textAlign="right"
-              >
-                <Button
-                  type="submit"
-                  background="#F2A443"
-                  color="white"
-                  _focus={{ shadow: "" }}
-                  fontWeight="md"
+                <Stack
+                  px={4}
+                  py={5}
+                  p={[null, 6]}
+                  bg={useColorModeValue("white", "gray.700")}
+                  spacing={6}
                 >
-                  Create
-                </Button>
-              </Box>
-            </chakra.form>
-          </GridItem>
-        </SimpleGrid>
+
+                  <Heading as='h3' size='md' mb="-20px" color="grey" fontSize='20px'>
+                    Plan an Audit
+                  </Heading>
+                  
+                  <SimpleGrid columns={6} spacing={6}>
+
+                    {/* 1th row input form  */}
+
+                    <FormControl as={GridItem} colSpan={[6, 3]}>
+                      <FormLabel
+                        htmlFor="first_name"
+                        fontSize="lg"
+                        fontWeight="xl"
+                        color={useColorModeValue("gray.700", "gray.50")}
+                      >
+                      </FormLabel>
+                      <Input
+                        type="text"
+                        name="first_name"
+                        id="first_name"
+                        autoComplete="given-name"
+                        mt={1}
+                        focusBorderColor="brand.400"
+                        shadow="sm"
+                        size="sm"
+                        w="full"
+                        rounded="md"
+                        placeholder='Start Date'
+
+                      />
+                      </FormControl>
+
+                    <FormControl as={GridItem} colSpan={[6, 3]}>
+                      <FormLabel
+                        htmlFor="last_name"
+                        fontSize="sm"
+                        fontWeight="md"
+                        color={useColorModeValue("gray.700", "gray.50")}
+                      >
+                        
+                      </FormLabel>
+                      <Input
+                        type="text"
+                        name="last_name"
+                        id="last_name"
+                        autoComplete="family-name"
+                        mt={1}
+                        focusBorderColor="brand.400"
+                        shadow="sm"
+                        size="sm"
+                        w="full"
+                        rounded="md"
+                        placeholder='End Date'
+
+                      />
+                    </FormControl>
+
+                    {/* 2th row input form  */}
+
+
+                    <FormControl as={GridItem} colSpan={[6, 3]}>
+                      <FormLabel
+                        htmlFor="first_name"
+                        fontSize="sm"
+                        fontWeight="md"
+                        color={useColorModeValue("gray.700", "gray.50")}
+                      >
+                       
+                      </FormLabel>
+                      <Input
+                        type="text"
+                        name="first_name"
+                        id="first_name"
+                        autoComplete="given-name"
+                        mt={1}
+                        focusBorderColor="brand.400"
+                        shadow="sm"
+                        size="sm"
+                        w="full"
+                        rounded="md"
+                        placeholder='Audit Type'
+
+                      />
+                      </FormControl>
+
+                    <FormControl as={GridItem} colSpan={[6, 3]}>
+                      <FormLabel
+                        htmlFor="last_name"
+                        fontSize="sm"
+                        fontWeight="md"
+                        color={useColorModeValue("gray.700", "gray.50")}
+                      >
+                        
+                      </FormLabel>
+                      <Input
+                        type="text"
+                        name="last_name"
+                        id="last_name"
+                        autoComplete="family-name"
+                        mt={1}
+                        focusBorderColor="brand.400"
+                        shadow="sm"
+                        size="sm"
+                        w="full"
+                        rounded="md"
+                        placeholder='Audit Sub-type'
+
+                      />
+                    </FormControl>
+
+
+                    {/* 3th row input form  */}
+                    
+
+                    <FormControl as={GridItem} colSpan={[6, 3]}>
+                      <FormLabel
+                        htmlFor="first_name"
+                        fontSize="sm"
+                        fontWeight="md"
+                        color={useColorModeValue("gray.700", "gray.50")}
+                      >
+                        
+                      </FormLabel>
+                      <Input
+                        type="text"
+                        name="first_name"
+                        id="first_name"
+                        autoComplete="given-name"
+                        mt={1}
+                        focusBorderColor="brand.400"
+                        shadow="sm"
+                        size="sm"
+                        w="full"
+                        rounded="md"
+                        placeholder='Department'
+
+                      />
+                      </FormControl>
+
+                    <FormControl as={GridItem} colSpan={[6, 3]}>
+                      <FormLabel
+                        htmlFor="last_name"
+                        fontSize="sm"
+                        fontWeight="md"
+                        color={useColorModeValue("gray.700", "gray.50")}
+                      >
+                      </FormLabel>
+                      <Input
+                        type="text"
+                        name="last_name"
+                        id="last_name"
+                        autoComplete="family-name"
+                        mt={1}
+                        focusBorderColor="brand.400"
+                        shadow="sm"
+                        size="sm"
+                        w="full"
+                        rounded="md"
+                        placeholder='Auditor Name'
+
+                      />
+                    </FormControl>
+                    {/* 4th row input form  */}
+                    <FormControl as={GridItem} colSpan={[6, 3]}>
+                      <FormLabel
+                        htmlFor="first_name"
+                        fontSize="sm"
+                        fontWeight="md"
+                        color={useColorModeValue("gray.700", "gray.50")}
+                      >
+                      </FormLabel>
+                      <Input
+                        type="text"
+                        name="first_name"
+                        id="first_name"
+                        autoComplete="given-name"
+                        mt={1}
+                        focusBorderColor="brand.400"
+                        shadow="sm"
+                        size="sm"
+                        w="full"
+                        rounded="md"
+                        placeholder='Country'
+
+                      />
+                      </FormControl>
+
+                    <FormControl as={GridItem} colSpan={[6, 3]}>
+                      <FormLabel
+                        htmlFor="last_name"
+                        fontSize="sm"
+                        fontWeight="md"
+                        color={useColorModeValue("gray.700", "gray.50")}
+                      >
+                      </FormLabel>
+                      <Input
+                        type="text"
+                        name="last_name"
+                        id="last_name"
+                        autoComplete="family-name"
+                        mt={1}
+                        focusBorderColor="brand.400"
+                        shadow="sm"
+                        size="sm"
+                        w="full"
+                        rounded="md"
+                        placeholder='Location'
+                        
+                      />
+                    </FormControl>
+
+                      
+                  </SimpleGrid>
+                <Textarea size='sm' placeholder='Note (Max40 Words)' />
+
+                </Stack>
+                <Box
+                  px={{ base: 4, sm: 6, lg: 6 }}
+                  py={3}
+                  bg={useColorModeValue("white", "gray.900")}
+                  textAlign="right"
+                >
+                  <Button
+                    type="submit"
+                    size='md'
+                    background="#F2A443"
+                    color="white"
+                    _focus={{ shadow: "" }}
+                    fontWeight="md"
+                    px="40px"
+                  >
+                    Create
+                  </Button>
+                </Box>
+              </chakra.form>
+            </GridItem>
+          </SimpleGrid>
+        </Box>
+
+      
+
+        
       </Box>
 
-     
-
-      
-    </Box>
+    </>
   );
 }
