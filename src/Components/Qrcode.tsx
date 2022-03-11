b991d0b9a867bb28925099bf5be8ba4fec744c48
PG <prajwalsmadhav@gmail.com>
2022-03-12 03:38:39 +0530

    updated qr page

--- src/Components/Qrcode.tsx
+++ src/Components/Qrcode.tsx
@@ -52,7 +52,7 @@ export default function Component() {
               direction={{ base: "row", md: "column" , lg: "column" }}
           >
             <Heading  mb={"4rem"} mt={-30} fontSize='30px'>
-              NC STATUS
+              Qr Code
             </Heading>
           </Flex>
 
