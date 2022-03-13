7580e00071bccb4d156ae187f23de32353a51d87
PG <prajwalsmadhav@gmail.com>
2022-03-13 20:26:08 +0530

    QR page update

--- src/Components/Qrcode.tsx
+++ src/Components/Qrcode.tsx
@@ -44,7 +44,7 @@ export default function Component() {
               direction={{ base: "row", md: "column" , lg: "column" }}
           >
             <Heading  mb={"4rem"} mt={-30} fontSize='30px'>
-              Qr Code
+              QR Code
             </Heading>
           </Flex>
 
