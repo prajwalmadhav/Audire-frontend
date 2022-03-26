70c2ee8d36503ae4a807d53e1d327612b7b9d7a5
PG <prajwalsmadhav@gmail.com>
2022-03-26 20:05:15 +0530

    api trial

---
+++ src/API/PostForm.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function PostForm() {
+  return (
+    <div>PostForm</div>
+  )
+}
+
+
+
+// import React, { Component } from 'react'
+
+// export class PostForm extends Component {
+
+//     constructor(props:any) {
+//       super(props)
+    
+//       this.state = {
+//          userEmail: '',
+//          userPassword:''
+
+//       }
+//     }
+
+//   render() {
+//     return (
+//       <div>
+//           <form onSubmit={handleSubmit}>
+//             <div>
+//                 <input
+//                  type="text" 
+//                  email="email"
+//                  password='userPassword'
+//                  value=''
+//                 ></input>
+//             </div>
+
+//           </form>
+//       </div>
+//     )
+//   }
+// }
+
+// export default PostForm
