8c5e37858849fc9a90c40d00af798ed5e5ea7dc3
PG <prajwalsmadhav@gmail.com>
2022-03-30 00:03:56 +0530

    update in api post request

--- src/API/PostForm.tsx
+++ src/API/PostForm.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react'
 
 export default function PostForm() {
@@ -5,8 +6,15 @@ export default function PostForm() {
     <div>PostForm</div>
   )
 }
-
-
+ 
+const userData = {
+  "userEmail": "guruvana@gmail.com",
+  "userPassword": "123@123"
+};
+axios.post("http://64.227.135.105/audire/api/login", userData).then((response) => {
+  console.log(response.status);
+  console.log(response.data.token);
+});
 
 // import React, { Component } from 'react'
 
