b834f2a60502cd1b7d34d52f1ec108d68cb02e93
PG <prajwalsmadhav@gmail.com>
2022-04-04 00:47:56 +0530

    Settings page update

--- src/Components/Settings.tsx
+++ src/Components/Settings.tsx
@@ -3,37 +3,32 @@ import {
     useColorModeValue,
     Stack,
     SimpleGrid,
-    Heading,
     IconButton,
     ButtonGroup,
-    Input,
-    InputGroup,
-    InputLeftElement,
-    HStack,
     Box,
-    InputRightElement,
     chakra,
+    Heading,
   } from "@chakra-ui/react";
   import { AiFillEdit } from "react-icons/ai";
-  import { Search2Icon } from "@chakra-ui/icons";
 import { FaRegTimesCircle } from "react-icons/fa";
   
   export default function Component() {
     const data = [
       { fullname: "Anderson Jeffe", dept: "Engineering", emailid : "anderson.jeffe@gmail.com", name: "Engineering", location: "Bengaluru"},
       { fullname: "Anderson Jeffe", dept: "Engineering", emailid : "anderson.jeffe@gmail.com", name: "Engineering", location: "Bengaluru"},
-      
-      
-        
-  
     ];
     const dataColor = useColorModeValue("white", "gray.800");
     const bg = useColorModeValue("white", "gray.800");
     const bg2 = useColorModeValue("gray.100", "gray.700");
   
     return (
-      
-      <Flex
+        <>
+        
+        <Heading mb={5} ml={'18%'} mt={10} fontSize='35px'>
+            Settings
+        </Heading>
+        
+        <Flex
             w="85%"
             bg="white"
             p={50}
@@ -42,119 +37,19 @@ import { FaRegTimesCircle } from "react-icons/fa";
             ml={210}
         >
 
-
-            <Stack
-                direction={{ base: "column" }}
-                w="full"
-                mr={5}
-                bg={{ md: bg }}
-                borderWidth='2px'
-
-
-
-            >
-                <Heading mb={10} mt={-30} fontSize='30px'>
-                        Settings
-                    </Heading>
-                <Flex
-                    direction={{ base: "row", md: "column", lg: "column" }}
-                >
-                    
-                </Flex>
-
-                <Stack>
-                    {/* // adding buttons and heading */}
-                </Stack>
-
-                <SimpleGrid
-                    spacingY={3}
-                    columns={{ base: 1, md: 3, lg: 7 }}
+                <Box
+                    // direction={{ base: "column" }}
+                    minH={'60vh'}
                     w="full"
-                    textTransform="uppercase"
-                    bg={bg2}
-                    color={"gray.500"}
-                    py={{ base: 1, md: 4, lg: 4 }}
-                    px={{ base: 2, md: 10, lg: 15 }}
-                    fontSize="md"
-                    fontWeight="hairline"
-                    textAlign={"center"}
-                    alignItems="center"
-                    justifyContent="center"
-                >
-
-                    {/* Users  */}
-                    <span>Full Name</span>
-                    <span>Dept</span>
-                    <span>Email ID</span>
-                    
-
-
-
-                </SimpleGrid>
-
-                    {data.map((audit, pid) => {
-                    return (
-                        <Flex
-                            direction={{ base: "row", md: "column", lg: "column" }}
-                            bg={dataColor}
-                            key={pid}
-                        >
-
-                            <SimpleGrid
-                                spacingY={3}
-                                columns={{ base: 1, md: 3, lg: 7 }}
-                                w="full"
-                                py={{ base: 2, md: 4, lg: 2 }}
-                                px={{ base: 2, md: 10, lg: 15 }}
-                                fontWeight="hairline"
-                                textAlign={"center"}
-
-                            >
-                                
-                                <span>{audit.fullname}</span>
-                                <span>{audit.dept}</span>
-                                 {/* //for overflow of objects in table header */}
-                                    <chakra.span
-                                    textOverflow="ellipsis"
-                                    overflow="hidden"
-                                    whiteSpace="nowrap"
-                                    >
-                                    {audit.emailid}
-                                    </chakra.span>
-
-
-                                <Flex justify={{ lg: "flex-start" }}>
-                                    <ButtonGroup size="sm" spacing={5}>
-                                       
-                                        {/* <span>{audit.status}</span> */}
-                                        <IconButton colorScheme="orange" variant="outline" icon={<AiFillEdit />} aria-label={" "} />
-                                        <IconButton colorScheme="orange" variant="ghost" icon={<FaRegTimesCircle />} aria-label={" "} />
-
-                                    </ButtonGroup>
-                                </Flex>
-
-                            </SimpleGrid>
-
-
-                        </Flex>
-                    );
-                })}
-            </Stack>
-
-            <Stack
-                    direction={{ base: "column" }}
-                    w="full"
+                    mr={5}
                     bg={{ md: bg }}
-                    mt = {0}
                     borderWidth='2px'
-
-
-
                 >
+
                     <Flex
                         direction={{ base: "row", md: "column", lg: "column" }}
                     >
-                        
+
                     </Flex>
 
                     <Stack>
@@ -163,7 +58,7 @@ import { FaRegTimesCircle } from "react-icons/fa";
 
                     <SimpleGrid
                         spacingY={3}
-                        columns={{ base: 1, md: 3, lg: 7 }}
+                        columns={{ base: 1, md: 3, lg: 4 }}
                         w="full"
                         textTransform="uppercase"
                         bg={bg2}
@@ -173,11 +68,15 @@ import { FaRegTimesCircle } from "react-icons/fa";
                         fontSize="md"
                         fontWeight="hairline"
                         textAlign={"center"}
+                        alignItems="center"
+                        justifyContent="center"
                     >
 
                         {/* Users  */}
-                        <span>Name</span>
-                        <span>Location</span>
+                        <span>Full Name</span>
+                        <span>Dept</span>
+                        <span>Email ID</span>
+
 
 
 
@@ -193,7 +92,7 @@ import { FaRegTimesCircle } from "react-icons/fa";
 
                                 <SimpleGrid
                                     spacingY={3}
-                                    columns={{ base: 1, md: 3, lg: 7 }}
+                                    columns={{ base: 1, md: 3, lg: 4 }}
                                     w="full"
                                     py={{ base: 2, md: 4, lg: 2 }}
                                     px={{ base: 2, md: 10, lg: 15 }}
@@ -201,12 +100,28 @@ import { FaRegTimesCircle } from "react-icons/fa";
                                     textAlign={"center"}
 
                                 >
-                                   
-                                    <span>{audit.name}</span>
-                                    <span>{audit.location}</span>
+
+                                    <span>{audit.fullname}</span>
+                                    <span>{audit.dept}</span>
+                                    {/* //for overflow of objects in table header */}
+                                    <chakra.span
+                                        textOverflow="ellipsis"
+                                        overflow="hidden"
+                                        whiteSpace="nowrap"
+                                    >
+                                        {audit.emailid}
+                                    </chakra.span>
 
 
-                                    
+                                    <Flex justify={{ lg: "flex-start" }}>
+                                        <ButtonGroup size="sm" spacing={5}>
+
+                                            {/* <span>{audit.status}</span> */}
+                                            <IconButton colorScheme="orange" variant="outline" icon={<AiFillEdit />} aria-label={" "} />
+                                            <IconButton colorScheme="orange" variant="ghost" icon={<FaRegTimesCircle />} aria-label={" "} />
+
+                                        </ButtonGroup>
+                                    </Flex>
 
                                 </SimpleGrid>
 
@@ -214,8 +129,166 @@ import { FaRegTimesCircle } from "react-icons/fa";
                             </Flex>
                         );
                     })}
-            </Stack>
+                </Box>
 
-        </Flex>         
+                <Box
+                    // direction={{ base: "column" }}
+                    minH={'60vh'}
+                    w="full"
+                    mr={5}
+                    bg={{ md: bg }}
+                    borderWidth='2px'
+                >
+
+                    <Flex
+                        direction={{ base: "row", md: "column", lg: "column" }}
+                    >
+
+                    </Flex>
+
+                    <Stack>
+                        {/* // adding buttons and heading */}
+                    </Stack>
+
+                    <SimpleGrid
+                        spacingY={3}
+                        columns={{ base: 1, md: 3, lg: 3 }}
+                        w="full"
+                        textTransform="uppercase"
+                        bg={bg2}
+                        color={"gray.500"}
+                        py={{ base: 1, md: 4, lg: 4 }}
+                        px={{ base: 2, md: 10, lg: 15 }}
+                        fontSize="md"
+                        fontWeight="hairline"
+                        textAlign={"center"}
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+
+                        {/* Users  */}
+                        <span>Name</span>
+                        <span>Location</span>
+
+                    </SimpleGrid>
+
+                    {data.map((audit, pid) => {
+                        return (
+                            <Flex
+                                direction={{ base: "row", md: "column", lg: "column" }}
+                                bg={dataColor}
+                                key={pid}
+                            >
+
+                                <SimpleGrid
+                                    spacingY={3}
+                                    columns={{ base: 1, md: 3, lg: 3 }}
+                                    w="full"
+                                    py={{ base: 2, md: 4, lg: 2 }}
+                                    px={{ base: 2, md: 10, lg: 15 }}
+                                    fontWeight="hairline"
+                                    textAlign={"center"}
+
+                                >
+
+                                    <span>{audit.name}</span>
+                                    <span>{audit.location}</span>
+                                    {/* //for overflow of objects in table header */}
+
+
+
+                                    <Flex justify={{ lg: "flex-start" }}>
+                                        <ButtonGroup size="sm" spacing={5}>
+
+
+
+                                        </ButtonGroup>
+                                    </Flex>
+
+                                </SimpleGrid>
+
+
+                            </Flex>
+                        );
+                    })}
+                </Box>
+
+                {/* <Box
+            // direction={{ base: "column" }}
+            w="full"
+            bg={{ md: bg }}
+            mt = {0}
+            borderWidth='2px'
+            minH={'50vh'}
+
+
+
+        >
+            <Flex
+                direction={{ base: "row", md: "column", lg: "column" }}
+            >
+                
+            </Flex>
+
+            
+
+            <SimpleGrid
+                spacingY={3}
+                columns={{ base: 1, md: 3, lg: 3 }}
+                w="full"
+                textTransform="uppercase"
+                bg={bg2}
+                color={"gray.500"}
+                py={{ base: 1, md: 4, lg: 4 }}
+                px={{ base: 2, md: 10, lg: 15 }}
+                fontSize="md"
+                fontWeight="hairline"
+                textAlign={"justify"}
+            >
+
+                
+                <span>Name</span>
+                <span>Location</span>
+
+
+
+            </SimpleGrid>
+
+            {data.map((audit, pid) => {
+                return (
+                    <Flex
+                        direction={{ base: "row", md: "column", lg: "column" }}
+                        bg={dataColor}
+                        key={pid}
+                    >
+
+                        <SimpleGrid
+                            spacingY={3}
+                            columns={{ base: 1, md: 3, lg: 3 }}
+                            w="full"
+                            py={{ base: 2, md: 4, lg: 2 }}
+                            px={{ base: 2, md: 10, lg: 15 }}
+                            fontWeight="hairline"
+                            textAlign={"center"}
+
+                        >
+                           
+                            <span>{audit.name}</span>
+                            <span>{audit.location}</span>
+
+
+                            
+
+                        </SimpleGrid>
+
+
+                    </Flex>
+                );
+            })}
+    </Box> */}
+
+
+
+            </Flex></>         
     );
   }
