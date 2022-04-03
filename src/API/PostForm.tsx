a1c0b05a4a326593a84184eae66b101f79f5b61c
PG <prajwalsmadhav@gmail.com>
2022-04-04 00:16:41 +0530

    updated post request api

--- src/API/PostForm.tsx
+++ src/API/PostForm.tsx
@@ -2,51 +2,18 @@ import axios from 'axios';
 import React from 'react'
 
 export default function PostForm() {
+  const userData = {
+    "userEmail": "guruvana@gmail.com",
+    "userPassword": "123@123"
+  };
+  
+
   return (
-    <div>PostForm</div>
+    
+      axios.post("http://64.227.135.105/audire/api/login", userData).then((response) => {
+      console.log(response.status);
+      console.log(response.data.token);
+      console.log("Success");
+    })
   )
 }
- 
-const userData = {
-  "userEmail": "guruvana@gmail.com",
-  "userPassword": "123@123"
-};
-axios.post("http://64.227.135.105/audire/api/login", userData).then((response) => {
-  console.log(response.status);
-  console.log(response.data.token);
-});
-
-// import React, { Component } from 'react'
-
-// export class PostForm extends Component {
-
-//     constructor(props:any) {
-//       super(props)
-    
-//       this.state = {
-//          userEmail: '',
-//          userPassword:''
-
-//       }
-//     }
-
-//   render() {
-//     return (
-//       <div>
-//           <form onSubmit={handleSubmit}>
-//             <div>
-//                 <input
-//                  type="text" 
-//                  email="email"
-//                  password='userPassword'
-//                  value=''
-//                 ></input>
-//             </div>
-
-//           </form>
-//       </div>
-//     )
-//   }
-// }
-
-// export default PostForm
