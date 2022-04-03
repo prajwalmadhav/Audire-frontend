4052f6350858730e8e8c05c4cb17c603e16dc2c5
PG <prajwalsmadhav@gmail.com>
2022-04-04 00:16:50 +0530

    dashboard button test

--- src/Components/Dashboard.tsx
+++ src/Components/Dashboard.tsx
@@ -1,13 +1,12 @@
 import { AddIcon } from '@chakra-ui/icons'
 import { Button, Stack } from '@chakra-ui/react'
-import React from 'react'
 
 export default function Homepage() {
   return (
       <>
 
         <Stack direction='row' spacing={4} mt="50px" ml="300px">
-          <Button leftIcon={<AddIcon />} background="#F2A143" color='White' variant='solid'>
+          <Button leftIcon={<AddIcon />} background="#F2A143" color='White' variant='solid' >
           Plan An Audit
           </Button>
         </Stack>
