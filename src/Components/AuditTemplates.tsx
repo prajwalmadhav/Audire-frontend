1e202d55f2e78b74a12c17a4ea177c2ff4c8508b
PG <prajwalsmadhav@gmail.com>
2022-04-06 15:07:46 +0530

    audit templates

--- src/Components/AuditTemplates.tsx
+++ src/Components/AuditTemplates.tsx
@@ -13,6 +13,7 @@ import {
     Box,
     InputRightElement,
     Badge,
+    Icon,
   } from "@chakra-ui/react";
   import {
     chakra,
@@ -21,11 +22,15 @@ import {
   } from "@chakra-ui/react";
   import { AiFillEdit } from "react-icons/ai";
   import { Search2Icon } from "@chakra-ui/icons";
-  
+import { FiEdit2 } from "react-icons/fi";
+const data = {
+  name: 'Add New Template',
+  date: 21,
+  numReviews: 34
+};
+
   export default function Component() {
-    const data = [
-      
-    ];
+   
     const dataColor = useColorModeValue("white", "gray.800");
     const bg = useColorModeValue("white", "gray.800");
     const bg2 = useColorModeValue("gray.100", "gray.700");
@@ -56,19 +61,7 @@ import {
 
             {/* searching fields */}
             <Stack  >
-              <HStack spacing={10} mb={20} ml={'50%'} bg="gray.100" rounded='lg' py={5}>
-                <Box >
-
-                  {/* <InputGroup>
-                    <InputRightElement
-                      pointerEvents='none'
-                      children={<Search2Icon color='gray.300' />}
-                    />
-                    <Input type='search' placeholder='Audit Type' />
-                  </InputGroup> */}
-
-                </Box>
-
+              <HStack spacing={10} mb={20} ml={'50%'} bg="gray.100" rounded='lg' pl={5} py={5}>
                 <Box >
                   <InputGroup bg='white' rounded='xl'>
                     <Input type='search' placeholder='Audit Type' />
@@ -88,10 +81,10 @@ import {
               columns={{sm: 2, md: 3, lg: 4}} 
               spacing='60px'>
                 {/* First qr box  */}
-              <Box fontSize='xl' alignItems='center' justifyContent='center'>
+              <Flex fontSize='xl' alignItems='center' ml={{base:0,lg:'15%'}} justifyContent='center'>
                 {/* Compailance Audit */}
               
-                <Box bg='gray.50'
+                {/* <Box bg='gray.50'
                   display='flex' 
                   maxW='lg'
                   alignItems='center'
@@ -99,15 +92,48 @@ import {
                   h='250px'
                   borderWidth='3px' 
                   borderRadius='50' 
-                  overflow='hidden' > 
+                  overflow='hidden' >
 
-                    <Stack >
+                </Box> */}
+                <Box
+          bg={useColorModeValue('white', 'gray.800')}
+          maxW="sm"
+          borderWidth="4px"
+          borderColor="#707070"
+          rounded="40px"
+          shadow="sm"
+          position="relative">
+                      
+          <Box p="6">
+            
+            <Stack mt="1" justifyContent="space-between" alignContent="center">
+              <Box
+                fontSize="xl"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                isTruncated>
+                {data.name}
+              </Box>
 
+              <Box fontSize="lg" color={useColorModeValue('gray.800', 'white')}>
+                
+                {data.date.toFixed(2)}
+              </Box>
 
-                    </Stack>
-
-                </Box>
-              </Box>             
+              <Box
+                alignItems="center"
+                fontSize={'1.2em'}
+                >
+                <chakra.a href={'#'} display={'flex'} >
+                  <Icon as={FiEdit2} h={7} w={7} alignSelf={'center'} />
+                </chakra.a>
+              </Box>
+            </Stack>
+          </Box>
+        </Box>
+        
+              </Flex>             
                        
             </SimpleGrid>
   
